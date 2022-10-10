9c901524acf833f7229a016b7488cd61fc81a6b4
Erivan Pereira <gasparzinho633@gmail.com>
2022-10-10 10:49:28 -0300

    created delete view

--- base/views.py
+++ base/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.views import View
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
 from .models import Task
@@ -27,4 +27,9 @@ class TaskCreate(CreateView):
 class TaskUpdate(UpdateView):
     model = Task
     fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+class TaskDelete(DeleteView):
+    model = Task
+    context_object_name = 'task'
     success_url = reverse_lazy('tasks')
